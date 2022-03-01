bcd2d7bcad9f35aabf2f575ec93c3123edf967e1
lucasalx <lucasaleixofirmino@gmail.com>
2022-03-01 17:43:31 -0300

    edditing "index.js" file

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const chalk = require('chalk');
 
-const msg = chalk.green('Hello World');
-console.log(msg);
+const msgRed = chalk.red('This text is red');
+console.log(msgRed);
+
+const msgGreen = chalk.green('This text is green');
+console.log(msgGreen);
+
+const msgBlue = chalk.blue('This text is blue');
+console.log(msgBlue);
